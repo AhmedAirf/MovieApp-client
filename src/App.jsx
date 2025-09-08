5fee8d36397527bd1a814b4ef786ee348732b335
AhmedAirf <ahmed.airf.x@gmail.com>
2025-09-08 19:47:47 +0300

    routing scroll issue fixed

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import React from "react";
 import AppRoutes from "../routes";
 import AuthInitializer from "./components/common/AuthInitializer";
 import ThemeInitializer from "./components/common/ThemeInitializer";
+import ScrollToTop from "./components/common/ScrollToTop";
 
 const App = () => {
   return (
     <>
       <ThemeInitializer />
       <AuthInitializer />
+      <ScrollToTop />
       <AppRoutes />
     </>
   );
